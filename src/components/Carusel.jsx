38cf969187e04e97093514f73f934ff16a1190a6
abrorjon0990 <abdurahimovabrorjon346@gmail.com>
2025-03-09 09:10:21 +0300

    togirlandi

--- src/components/Carusel.jsx
+++ src/components/Carusel.jsx
@@ -1,52 +1,52 @@
 import { useEffect, useState } from "react";
+import rasm1 from "../image/rasm1.jpg";
+import rasm2 from "../image/rasm2.jpg";
+import rasm3 from "../image/rasm3.jpg";
+import rasm4 from "../image/rasm4.jpg";
+import rasm5 from "../image/rasm5.jpg";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
+
 
 const Carousel = () => {
-  const slides = [
-    "https://images.uzum.uz/cv3eg7ei4n36ls3t0770/main_page_banner.jpg",
-    "https://images.uzum.uz/cug7q9tht56sc95cis1g/main_page_banner.jpg",
-    "https://images.uzum.uz/cv4o265pb7f9qcng1frg/main_page_banner.jpg",
-    "https://images.uzum.uz/cuuoplei4n36ls3rla6g/main_page_banner.jpg",
-    "https://images.uzum.uz/cuuljv3vgbkm5ehgnhcg/main_page_banner.jpg",
-  ];
-
+  const slides = [rasm1, rasm2, rasm3, rasm4, rasm5];
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % slides.length);
     }, 3500);
-
     return () => clearInterval(interval);
-  }, []);
+  }, [slides.length]);
 
   return (
-    <div className="carousel w-full mt-10 rounded-md">
+    <div className="relative w-full mt-10  mb-[480px]">
       {slides.map((slide, index) => (
-        <div
+        <img
           key={index}
-          className={`carousel-item relative w-full ${
-            index === current ? "block" : "hidden"
+          src={slide}
+          className={`absolute top-0 left-0 w-full transition-opacity duration-700 ${
+            index === current ? "opacity-100" : "opacity-0"
           }`}
-        >
-          <img src={slide} className="w-full" alt={`Slide ${index + 1}`} />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <button
-              onClick={() =>
-                setCurrent((prev) => (prev - 1 + slides.length) % slides.length)
-              }
-              className="btn btn-circle"
-            >
-              ❮
-            </button>
-            <button
-              onClick={() => setCurrent((prev) => (prev + 1) % slides.length)}
-              className="btn btn-circle"
-            >
-              ❯
-            </button>
-          </div>
-        </div>
+          alt={`Slide ${index + 1}`}
+        />
       ))}
+      <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+        <button
+          onClick={() =>
+            setCurrent((prev) => (prev - 1 + slides.length) % slides.length)
+          }
+          className="btn btn-circle mt-[400px]"
+        >
+          <GoChevronLeft size={35} />
+
+        </button>
+        <button
+          onClick={() => setCurrent((prev) => (prev + 1) % slides.length)}
+          className="btn btn-circle mt-[400px]"
+        >
+          <GoChevronRight size={35} />
+        </button>
+      </div>
     </div>
   );
 };
